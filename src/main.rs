4fbece0b37555cba3059645ae8924f359ca93025
Ross Nkama <ssor.nkama@gmail.com>
2023-12-09 23:11:01 +0400

    Attempt 1: Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    error::Error,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
 };
@@ -22,42 +23,42 @@ impl Message for ResponseKind {
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    if let Err(e) = run_server() {
+        eprintln!("error: {}", e);
+    }
+}
+
+fn run_server() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:4221")?;
 
     println!("Server up!");
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                handle_connection(_stream);
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+        if let Ok(stream) = stream {
+            handle_connection(stream)?;
         }
     }
+
+    Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     let mut buffer = [0; 512];
-    let response: ResponseKind;
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer)?;
     let request = String::from_utf8_lossy(&buffer);
     println!("Request: {} \n\n *** *** *** *** \n", request);
 
-    let request_header = resolve_path(&request);
-    
-    if request_header[1] == "/" {
-        response = ResponseKind::Ok
-    } else {
-        response = ResponseKind::NotFound
-    }
-    
-    stream.write(response.get_message()).unwrap();
-    stream.flush().unwrap();
+    let response = match resolve_path(&request) {
+        Some("/") => ResponseKind::Ok,
+        _ => ResponseKind::NotFound,
+    };
+
+    stream.write(response.get_message())?;
+    stream.flush()?;
+    Ok(())
 }
 
-fn resolve_path(request: &str) -> Vec<&str> {
-    let request_header = request.lines().next().unwrap_or("");
-    request_header.split_whitespace().collect()
+fn resolve_path(request: &str) -> Option<&str> {
+    let request_header = request.lines().next()?;
+    request_header.split_whitespace().nth(1)
 }
